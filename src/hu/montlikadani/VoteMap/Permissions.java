62f5b172f2f7d9fa16f071f313a4524ef597f842
montlikadani <montlikada@gmail.com>
2018-06-23 14:51:41 +0200

    Add files via upload

---
+++ src/hu/montlikadani/VoteMap/Permissions.java
@@ -0,0 +1,14 @@
+package hu.montlikadani.VoteMap;
+
+import org.bukkit.permissions.Permission;
+
+public class Permissions {
+
+	public static final Permission PINFO = new Permission("votemap.plugininfo");
+	public static final Permission HELP = new Permission("votemap.help");
+	public static final Permission RELOAD = new Permission("votemap.reload");
+	public static final Permission PDISABLE = new Permission("votemap.plugindisable");
+	public static final Permission TABCOMP = new Permission("votemap.cmd.tabcomplete");
+	public static final Permission VOTE = new Permission("votemap.vote");
+
+}
